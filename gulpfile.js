e5048263e8773ecbbb2879ca2361f6157f969301
gtkatakura-bysoft <gabriel.takashi@bysoft.com.br>
2016-02-03 13:00:18 -0200

    [ORGANIZE] Estrutura de pastas modificada

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,19 @@ var gulp = require('gulp'),
     concat = require('gulp-concat');
     
 var JAVASCRIPT_SRC = [
-        'js/commandDecorator.js',
-        'js/commandRepository.js',
-        'js/commands/*.js',
-        'js/terminal.js',
-        'js/terminalFactory.js',
-        'js/main.js'
+        'app/js/terminal/commandDecorator.js',
+        'app/js/terminal/commandRepository.js',
+        'app/js/terminal/commands/*.js',
+        'app/js/terminal/terminal.js',
+        'app/js/terminal/terminalFactory.js',
+        'app/js/view/index.js'
     ];
     
 gulp.task('scripts', function()
 {
     return gulp.src(JAVASCRIPT_SRC)
         .pipe(concat('app.js'))
-        .pipe(gulp.dest('js'));
+        .pipe(gulp.dest('app/js'));
 });
 
 gulp.task('watch', function()
