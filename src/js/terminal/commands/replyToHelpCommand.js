6c032a9d381645568e9d058ed58c168fcb9c5512
Pedro Laxe <pedrolaxe@gmail.com>
2016-02-17 17:44:36 -0200

    update help file to add iiissa command

--- src/js/terminal/commands/replyToHelpCommand.js
+++ src/js/terminal/commands/replyToHelpCommand.js
@@ -3,15 +3,16 @@
         name: 'help',
         aliases: [ '?' ],
         response: [
-            '- Open + website URL to open it in the browser (ex. open phpsec.com.br)',
-            '- Google + keyword to search directly in Google (ex. google phpsec)',
-            '- Yahoo + keyword to search directly in Yahoo (ex. yahoo dogs)',
-            '- YouTube + keyword to search directly in YouTube (ex. youtube fora pt)',
-            '- Wiki + keyword to search directly in Wikipedia (ex. wiki javascript)',
-            '- \'Time\' will display the current time.',
-            '- \'Date\' will display the current date.',
-            '- \'creators\' show the creators names.',
-            '* There are more keywords that you have to discover by yourself.'
+          '- Open + website URL to open it in the browser (ex. open phpsec.com.br)',
+          '- Google + keyword to search directly in Google (ex. google phpsec)',
+          '- Yahoo + keyword to search directly in Yahoo (ex. yahoo dogs)',
+          '- iiissa + keyword to search directly in IIISSA (ex. iiissa pedro)',
+          '- YouTube + keyword to search directly in YouTube (ex. youtube fora pt)',
+          '- Wiki + keyword to search directly in Wikipedia (ex. wiki javascript)',
+          '- \'Time\' will display the current time.',
+          '- \'Date\' will display the current date.',
+          '- \'creators\' show the creators names.',
+          '* There are more keywords that you have to discover by yourself.'
         ]
     };
 })(window.Terminal);
