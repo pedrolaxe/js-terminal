44090e7d06a7f23bf89161f69bc362446e4c9305
gtkatakura <gt.katakura@gmail.com>
2017-03-25 18:13:10 -0300

    add(babel-preset-stage-0)

--- gulpfile.js/webpack.config.js
+++ gulpfile.js/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['es2015'],
+          presets: ['es2015', 'stage-0'],
           plugins: ['transform-runtime']
         }
       }
